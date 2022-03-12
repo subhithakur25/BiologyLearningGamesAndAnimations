17136fc1834627e07d46647a182bef43db768c1b
Prajwal Das <prajwaldas@Prajwals-MacBook-Pro.local>
2022-03-12 12:48:03 -0600

    Added Animation for 'Calculate the value' slide

---
+++ BiologyAnimation/InfectiousDiseases/Calculate the value/js/contents.js
@@ -0,0 +1,33 @@
+var questions = [
+{
+	"question id" : "1",
+    "answer id" : "a1",
+    "answer": "4.1"
+},
+
+{
+	"question id" : "2",
+    "answer id" : "a2",
+    "answer": "4"
+},
+
+{
+	"question id" : "3",
+    "answer id" : "a3",
+    "answer": "8"
+},
+
+{
+	"question id" : "4",
+    "answer id" : "a4",
+    "answer": "2"
+},
+
+
+
+];
+
+var slides_info = {
+	"topic":  "Measure of central tendency",
+    "hint": "Go back and read about central tendency from the analyzing data slides!"
+}

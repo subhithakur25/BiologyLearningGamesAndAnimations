9a9d9b97b88a37fc56ab2e994e71bfafce4a0921
nimisha <nimishasrivastava14@gmail.com>
2022-03-20 13:02:26 -0500

    User story right axis code changes

---
+++ BiologyAnimation/InfectiousDiseases/WhichAxisIsRight/js/contents.js
@@ -0,0 +1,37 @@
+var questions = [
+{
+	"question id" : "1",
+    "answer id" : "a1",
+    "answer": "dependent & y-axis"
+},
+
+{
+	"question id" : "2",
+    "answer id" : "a2",
+    "answer": "independent & x-axis"
+},
+
+{
+	"question id" : "3",
+    "answer id" : "a3",
+    "answer": "dependent & x-axis"
+},
+
+{
+	"question id" : "4",
+    "answer id" : "a4",
+    "answer": "independent & y-axis"
+},
+
+{
+	"question id" : "5",
+    "answer id" : "a5",
+    "answer": "None of the above"
+},
+
+
+];
+
+var slides_info = {
+    "hint": "Go back and read about Types of Data:Graphs!"
+}

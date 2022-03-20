009c98cddc367d994dc7ca81a54f33c2e3c5574e
Apurva <54936708+ApurvaP-97@users.noreply.github.com>
2022-03-19 22:08:58 -0500

    Added Animation : Name that variable Slide

---
+++ BiologyAnimation/InfectiousDiseases/Name that variable/js/contents.js
@@ -0,0 +1,25 @@
+var questions = [
+{
+	"question id" : "1",
+    "answer id" : "a1",
+    "answer": "Independent"
+},
+
+{
+	"question id" : "2",
+    "answer id" : "a2",
+    "answer": "Controlled"
+},
+
+{
+	"question id" : "3",
+    "answer id" : "a3",
+    "answer": "Dependent"
+},
+
+];
+
+var slides_info = {
+	"topic":  "Types of variables",
+    "hint": "Go back and review the types of variables."
+}

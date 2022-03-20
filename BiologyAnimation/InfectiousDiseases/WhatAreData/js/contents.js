92d7fc3d9e7e656e302d9565a4e13dd3a07dbc6b
Sai Harini Voruganti <saiharinivoruganti@Sais-MacBook-Pro.local>
2022-03-19 21:37:06 -0500

    whatAreData

---
+++ BiologyAnimation/InfectiousDiseases/WhatAreData/js/contents.js
@@ -0,0 +1,24 @@
+var questions = [
+{
+	"question id" : "1",
+    "answer id" : "a1",
+    "answer": "observations"
+},
+
+{
+	"question id" : "2",
+    "answer id" : "a2",
+    "answer": "facts"
+},
+
+{
+	"question id" : "3",
+    "answer id" : "a3",
+    "answer": "studying"
+},
+];
+
+var slides_info = {
+	"topic":  "What are data",
+    "hint": "Go back and read the data collection and organization slides!"
+}
